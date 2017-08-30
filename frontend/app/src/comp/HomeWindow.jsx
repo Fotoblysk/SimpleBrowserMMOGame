f87cfaf1573e03de95194b50a15d37080abc39b4
Fotoblysk <fotoblysk@fejm.pl>
2017-08-31 00:23:28 +0200

    Added react-router-dom, changed navibar.

---
+++ frontend/app/src/comp/HomeWindow.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AboutWindow extends Component {
+
+  render() {
+    return (
+      <div>
+        Home.
+      </div>
+    );
+  }
+}
+
+export default AboutWindow;
---
+++ frontend/app/src/func/isLogged.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+class LoginChecker extends Component {
+  checkLogin = () {
+    a
+  }
+}
