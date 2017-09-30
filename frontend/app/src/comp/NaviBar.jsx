4013ce0a6cee88eacbb727a381f32850eb3b3531
Fotoblysk <fotoblysk@fejm.pl>
2017-09-30 15:10:56 +0200

    Added react=router-bootstrap changed navibar to responsive bootstrap navibar.

--- frontend/app/src/comp/NaviBar.jsx
+++ frontend/app/src/comp/NaviBar.jsx
@@ -1,33 +1,40 @@
 import React, { Component } from "react";
 import "../css/NaviBar.css";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap';
 
 class NaviBar extends Component {
 
   render() {
     return (
-      <div className="navi-bar">
-        &nbsp;
-        <Link to={"/"} className="supress-link-style inline">
-          <span className="link">Home</span>
-        </Link>
-        &nbsp;
-        <Link to={"/login"} className="supress-link-style inline">
-          <span className="link">Login</span>
-        </Link>
-        &nbsp;
-        <Link to={"/register"} className="supress-link-style inline">
-          <span className="link">Register</span>
-        </Link>
-        &nbsp;
-        <Link to={"/about"} className="supress-link-style inline">
-          <span className="link">About</span>
-        </Link>
-        &nbsp;
-        <Link to={"/dashboard"} className="supress-link-style inline">
-          <span className="link">Dashboard</span>
-        </Link> 
-        &nbsp;
+      <div>
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <LinkContainer eventKey={1} to={"/"}>
+                  <a>Browser Game</a>
+                </LinkContainer>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <LinkContainer to={"/login"}>
+                  <NavItem eventKey={2}>Login</NavItem>
+                </LinkContainer>
+                <LinkContainer to={"/register"}>
+                  <NavItem eventKey={3}>Register</NavItem>
+                </LinkContainer>
+                <LinkContainer to={"/about"}>
+                  <NavItem eventKey={4}>About</NavItem>
+                </LinkContainer>
+                <LinkContainer to={"/dashboard"}>
+                  <NavItem eventKey={5}>Dashboard</NavItem>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
       </div>
     );
   }
